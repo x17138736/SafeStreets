eb8e532df21f1d3a9bb4e8c475d3a93bc6889df6
Oscar McGovern O'Byrne <36077674+x173636253@users.noreply.github.com>
2019-11-04 01:29:09 +0000

    Add files via upload

---
+++ SafeStreets/app/src/main/java/com/example/safestreets/Settings.java
@@ -0,0 +1,70 @@
+package com.example.safestreets;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.provider.ContactsContract;
+import android.view.View;
+import android.widget.Button;
+
+public class Settings extends AppCompatActivity {
+
+    private Button Contacts;
+    private Button TermsOfService;
+    private Button SignOut;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_settings);
+
+        Contacts = (Button) findViewById(R.id.btnContactsPage);
+        Contacts.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openContacts();
+            }
+        });
+    }
+
+    public void openContacts() {
+        Intent intent = new Intent(this, ContactsContract.Contacts.class);
+        startActivity(intent);
+
+        TermsOfService = (Button) findViewById(R.id.btnTermsOfService);
+        TermsOfService.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openTermsOfService();
+            }
+        });
+    }
+
+    public void openTermsOfService() {
+        Intent intent = new Intent(this, TermsOfService.class);
+        startActivity(intent);
+
+
+        SignOut = (Button) findViewById(R.id.btnSignOut);
+        SignOut.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openMainActivity();
+            }
+        });
+
+
+    }
+
+    public void openMainActivity() {
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+    }
+
+    @Override
+    public void onBackPressed() {
+        //to nothing       }
+    }
+
+}
---
+++ SafeStreets/app/src/main/java/com/example/safestreets/TermsOfService.java
@@ -0,0 +1,14 @@
+package com.example.safestreets;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+class TermsOfService extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_terms_of_service);
+    }
+}
