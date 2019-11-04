4f2010548ee5ec3f1280565b3d1be00aa3b6700a
owen197 <x17516666@student.ncirl.ie>
2019-11-04 12:31:12 +0000

    Contacts/ToS/SignOut Functioning

--- app/src/main/java/com/example/safestreets/Settings.java
+++ app/src/main/java/com/example/safestreets/Settings.java
@@ -9,16 +9,64 @@ import android.widget.Button;
 
 public class Settings extends AppCompatActivity {
 
+    private Button Contacts;
+    private Button TermsOfService;
+    private Button SignOut;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
+
+        Contacts = (Button) findViewById(R.id.btnContactsPage);
+        Contacts.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openContacts();
+            }
+        });
     }
 
+    public void openContacts() {
+        Intent intent = new Intent(this, Contacts.class);
+        startActivity(intent);
 
+        TermsOfService = (Button) findViewById(R.id.btnToS);
+        TermsOfService.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openTermsOfService();
+            }
+        });
+    }
+
+    public void openTermsOfService() {
+        Intent intent = new Intent(this, TermsOfService.class);
+        startActivity(intent);
+
+
+        SignOut = (Button) findViewById(R.id.btnSignOut);
+        SignOut.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openMainActivity();
+            }
+        });
 
 
     }
+
+    public void openMainActivity() {
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+    }
+
+    @Override
+    public void onBackPressed() {
+        //to nothing       }
+    }
+
+}
 
 
 
